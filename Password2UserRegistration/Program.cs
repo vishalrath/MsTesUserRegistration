d2bb05b097a519456335a1a048cfbed37f9cf07a
Vishal@7038 <vishalrathod7038@gmail.com>
2021-11-15 23:35:14 +0530

    [vishal] Uc6 MsTest of Password1 validation adding 1 uppercase of UserRegistration

---
+++ Password2UserRegistration/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Password2UserRegistration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
